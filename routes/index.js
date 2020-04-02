56667ee0c8e40b44b25e784356cfafa4282f9a80
Tommy Kearns <Omnighost547@users.noreply.github.com>
2020-04-02 01:21:17 +0100

    Home and Login pages
    * Home and Login pages
    
    * Home and Login pages
    
    Co-authored-by: Jack Madden <g00338579@gmit.ie>

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const mongo = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,20 @@ router.get('/', function(req, res, next) {
 
 router.get('/bike', function(req, res, next) {
   res.render('bike', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Welcome!' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Log in' });
+});
+
+router.post('/login/submit', function(req, res, next) {
+  var name = req.body.id;
+  var password = req.body.id;
+  res.redirect('/home');
 });
 
 module.exports = router;
